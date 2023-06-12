18fc6228942aa080fdb0d0a8560166677d592dc8
jeremyimbisi <jeremyimbisi44@gmail.com>
2023-06-12 12:18:35 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+const names = ["Ada", "Brendan", "Ali"];
+const greeting = "birthday";
+
+function writeCards(names, greeting) {
+    const messages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const name = names[i];
+      const message = `Thank you, ${name}, for the wonderful ${greeting} gift!`;
+      messages.push(message);
+    }
+  
+    return messages;
+  }
+  
+ 
+const birthdayMessages = writeCards(names, greeting);
+
+console.log(birthdayMessages);
+
+
+function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  countDown(10)
